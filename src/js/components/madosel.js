0189b40c530da507b333e4665d1de15d543798a5
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-17 21:36:39 +0300

    Added madosel.js

---
+++ src/js/components/madosel.js
@@ -0,0 +1,12 @@
+/*!
+ * Madosel 1.0.0-alpha3
+ * Copyright 2020-2021 Kenan Gündoğan
+ * https://www.madosel.com
+ * Licensed under MIT https://github.com/kenangundogan/madosel/blob/master/LICENSE
+ * Released under the MIT License
+ */
+
+require('./madosel-accordion.js');
+require('./madosel-navigation-tabs.js');
+require('./madosel-modal.js');
+require('./madosel-offcanvas.js');
