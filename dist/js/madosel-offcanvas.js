3d57b6a2513bf679902884dbbf54990277211e44
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-25 14:26:17 +0300

    Update version (.js)

--- dist/js/madosel-offcanvas.js
+++ dist/js/madosel-offcanvas.js
@@ -1,10 +1,10 @@
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 var __webpack_exports__ = {};
 /*!********************************************!*\
   !*** ./js/components/madosel-offcanvas.js ***!
   \********************************************/
 /*!
- * Madosel Offcanvas 1.0.0-alpha6
+ * Madosel Offcanvas 1.0.0-alpha7
  * Copyright 2020-2021 Kenan Gündoğan
  * https://www.madosel.com
  * Licensed under MIT https://github.com/kenangundogan/madosel/blob/master/LICENSE
